5c5a6758507a6c2a1d8442b322cd462c20e489a7
colabLearn <tunji4physics@gmail.com>
2023-02-18 13:51:23 +0000

    DSA Exercise 3 - Parenthesis Checker: Given strings of parentheses, the checker return true if the parenthesiis are balance and false if it is not.

--- src/DSA_Exercise.java
+++ src/DSA_Exercise.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DSA_Exercise {
@@ -26,7 +28,6 @@ public class DSA_Exercise {
      *
      */
 
-
     public static void numberOfOccurence(String [] input){
         Map<String, Integer> myMap = new HashMap<>();
         for (String s : input) {
@@ -41,5 +42,79 @@ public class DSA_Exercise {
            System.out.printf("%s: %s \n", Key, myMap.get(Key));
        }
 
+    }
+
+    /*
+    * Exercise 2:
+    * Most Number of Occurrences
+    * -- Given the following array of strings. String[] input = "1,1,1,1,3,4,5,6,7,7,7,8,8";
+    * -- Find the element that has the most number of occurrences
+    * -- Your method should return something like the 1 since it occurs 4 times
+    * -- Think about the data structure that you should use
+    * -- Create a method. Think about the return type and parameters
+    * -- Write unit tests for your method
+    */
+
+    public static String mostNumOfOccurence(String [] input){
+        Map<String, Integer> myMap = new HashMap<>();
+        for (String s : input) {
+            if(myMap.containsKey(s)){
+                myMap.put(s, myMap.get(s)+1);
+            }else{
+                myMap.put(s, 1);
+            }
+        }
+        String most=null;
+        int max = 0;
+        for (String Key : myMap.keySet()){
+            if(myMap.get(Key)>max){
+                max = myMap.get(Key);
+                most = Key;
+            }
+        }
+        return most;
+    }
+
+    /*
+    * Exercise 3
+    * Parenthesis Checker Given the following inputs
+    * String brackets = "[{{[(){}]}}[]{}{{(())}}]";
+    * String brackets = "[{{}}[]{}{{(())}}]";
+    * String brackets = "[{}{}{{()}}]";
+    * String brackets = "[{}{{}}]";
+    * String brackets = "[{{}}]";
+    * String brackets = "[{}]";
+    * String brackets = "";
+    * Check if the following parenthesis are balanced. i.e
+    * [] - true
+    * [[] - false
+    * ({[]}) - true
+    * ({[}) - false
+    */
+
+    public static boolean parenthesisChecker(String parenthesis){
+        Map<String, String> parMap =
+                new HashMap<>();
+        parMap.put("(", ")");
+        parMap.put("[", "]");
+        parMap.put("{", "}");
+        parMap.put(")", "(");
+        parMap.put("]", "[");
+        parMap.put("}", "{");
+
+        List<String> myPar = new ArrayList<>();
+        String [] myChars = parenthesis.split("");
+        for (String myChar : myChars) {
+            if(myPar.isEmpty()){
+                myPar.add(myChar);
+            }else{
+                if(myPar.contains(parMap.get(myChar))) {
+                    myPar.remove(parMap.get(myChar));
+                }else{
+                    myPar.add(myChar);
+                }
+            }
+        }
+        return myPar.isEmpty();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
     public static void main(String[] args) {
 
@@ -6,5 +7,40 @@ public class Main {
          */
         String [] input = {"1","1","1","1","3","4","5","6","7","7","7","8","8"};
         DSA_Exercise.numberOfOccurence(input);
+
+        /*
+        * Output of DSA_Exercise.numberOfOccurence(input);
+        * 1: 4
+        * 3: 1
+        * 4: 1
+        * 5: 1
+        * 6: 1
+        * 7: 3
+        * 8: 2
+         */
+
+        /*
+        * Test implmentmentation of mostNumOfOccurence in Main
+        * This should return 1 as this is most number of occrence iin the array
+         */
+        System.out.println(DSA_Exercise.mostNumOfOccurence(input));
+
+        /*
+        * Output of - System.out.println(DSA_Exercise.mostNumOfOccurence(input));
+        *  1
+         */
+
+        //Creating and initializing String parenthesis
+        String par1 = "([[]])";
+        String par2 = "{[[{()}]]";
+        System.out.println(DSA_Exercise.parenthesisChecker(par1));
+        System.out.println(DSA_Exercise.parenthesisChecker(par2));
+
+        /*
+        * Output of:  System.out.println(DSA_Exercise.parenthesisChecker(par1));
+        *             System.out.println(DSA_Exercise.parenthesisChecker(par2));
+        * true
+        * false
+         */
     }
 }
