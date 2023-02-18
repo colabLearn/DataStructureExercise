c51dfed4e651fc351c700a93086d71de6c3da320
colabLearn <tunji4physics@gmail.com>
2023-02-18 13:51:23 +0000

    DSA Exercise 2 - Most Number Of Occurences

--- src/DSA_Exercise.java
+++ src/DSA_Exercise.java
@@ -42,4 +42,35 @@ public class DSA_Exercise {
        }
 
     }
+
+    /*
+    * Exercise 2:
+    * Most Number of Occurrences
+    * -- Given the following array of strings. String[] input = "1,1,1,1,3,4,5,6,7,7,7,8,8";
+    * -- Find the element that has the most number of occurrences
+    * -- Your method should return something like the 1 since it occurs 4 times
+    * -- Think about the data structure that you should use
+    * -- Create a method. Think about the return type and parameters
+    * -- Write unit tests for your method
+     */
+
+    public static String mostNumOfOccurence(String [] input){
+        Map<String, Integer> myMap = new HashMap<>();
+        for (String s : input) {
+            if(myMap.containsKey(s)){
+                myMap.put(s, myMap.get(s)+1);
+            }else{
+                myMap.put(s, 1);
+            }
+        }
+        String most=null;
+        int max = 0;
+        for (String Key : myMap.keySet()){
+            if(myMap.get(Key)>max){
+                max = myMap.get(Key);
+                most = Key;
+            }
+        }
+        return most;
+    }
 }
